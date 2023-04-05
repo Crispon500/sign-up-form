81268f33118b0ec8660d7ef8582cfbcb55ce0eba
Crispon500 <crispon500@gmail.com>
2023-04-05 10:09:20 -0500

    add phone number input onfocus function

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
-function addDashes(phone) {
-    if(phone.value.length > 3){
-        phone.value = phone.value.replace(/\D/g, '');
-        phone.value = phone.value.slice(0,3)+"-"+phone.value.slice(3,15);
-        if(phone.value.length > 7){
-            phone.value = phone.value.slice(0,7)+"-"+phone.value.slice(7,16);
+function addDashes(str) {
+    if(str.value.length > 3){
+        str.value = str.value.replace(/\D/g, '');
+        str.value = str.value.slice(0,3)+"-"+str.value.slice(3,15);
+        if(str.value.length > 7){
+            str.value = str.value.slice(0,7)+"-"+str.value.slice(7,16);
         }
     }
     else{
-        phone.value = phone.value.replace(/\D/g, '');
+        str.value = str.value.replace(/\D/g, '');
     }
            
+}
+
+function removeDashes(str) {
+    str.value = str.value.replace(/\D/g, '');
 }
