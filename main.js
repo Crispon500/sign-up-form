4744807fa606fc126066a8b81924f951656f15a7
Crispon500 <crispon500@gmail.com>
2023-04-05 10:03:03 -0500

    improve the dash fill for the phone number input

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 function addDashes(phone) {
-    phone.value = phone.value.replace(/\D/g, '');
-    phone.value = phone.value.slice(0,3)+"-"+phone.value.slice(3,6)+"-"+phone.value.slice(6,15);
+    if(phone.value.length > 3){
+        phone.value = phone.value.replace(/\D/g, '');
+        phone.value = phone.value.slice(0,3)+"-"+phone.value.slice(3,15);
+        if(phone.value.length > 7){
+            phone.value = phone.value.slice(0,7)+"-"+phone.value.slice(7,16);
+        }
+    }
+    else{
+        phone.value = phone.value.replace(/\D/g, '');
+    }
            
 }
